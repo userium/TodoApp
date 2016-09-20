0d670e19132126e54f7b1920c1552647c9d58324
userium <nina@userium.com>
2016-09-20 12:42:51 +0300

    added delete buttons for new items on the list

--- todo.js
+++ todo.js
@@ -2,15 +2,18 @@
 var todoList = {
 	todo: [],
 	addTodos: function() {
+
         // get the value from the input field
         var todoText = document.getElementById("addTodoInput").value;
         // add todotext on todo's array
         this.todo.push(todoText);
-        console.log("here is my todo:" + todoText);
-        document.getElementById("listOfTodos").innerHTML = '';
+        document.getElementById('listOfTodos').innerHTML = '';
         this.displayTodos();
+        this.createDeleteButton();
+        
 	},
 	displayTodos: function() {
+
 		this.todo.forEach(function(item) {
 			// get Ul element
 			var UlElement = document.getElementById("listOfTodos");
@@ -18,10 +21,12 @@ var todoList = {
 			var listTodos = document.createElement("li");
 			UlElement.appendChild(listTodos);
 			// add todo array item text inside the li element
+			listTodos.className = 'listTodos';
 			listTodos.innerHTML = item;	
 		});
 	},
 	deleteTodo: function() {
+
 		// get todo item array position
 		var getPosition = document.getElementById("deleteTodo").value;
 		// remove item from todo array
@@ -30,5 +35,14 @@ var todoList = {
         document.getElementById("listOfTodos").innerHTML = '';
         // display all todos from the todo array
 		this.displayTodos();
+	},
+	createDeleteButton: function() {
+		var deleteButton = document.createElement('button');
+		deleteButton.textContent = "delete";
+		deleteButton.className = 'deleteButton';
+		var addDeleteButton = document.getElementsByClassName('listTodos');
+		for(var i=0;i<addDeleteButton.length; i++) {
+			addDeleteButton[i].appendChild(deleteButton.cloneNode(true));
+		}
 	}
 }
